a76772d90895eff9e6fc05f43e1778fe4d29992d
gibanez <gei9980@gmail.com>
2019-06-25 16:37:39 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require('vendor/autoload.php');
+
+use UMSA\SidomModel\Model\User;
+
+$u = new User();
+var_dump($u);
---
+++ src/Model/User.php
@@ -0,0 +1,9 @@
+<?php
+namespace UMSA\SidomModel\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+
+}
